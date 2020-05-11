963218f7365c87a18e6059862b78f44c98fb0042
Erkinumur <erkin.umur94@gmail.com>
2020-05-11 20:13:48 +0600

    Update task

--- Exception_on_Decorator.py
+++ Exception_on_Decorator.py
@@ -1,31 +1,28 @@
 from functools import wraps
 
 
+exc = ''
 class User:
-    def balance_exc_decorator(func):
-        @wraps(func)
-        def wrapper(self):
-            try:
-                func(self)
-            except:
-                raise Exception('No username defined!')
-        return wrapper
-
-    def password_exc_decorator(func):
+    def exc_decorator(func):
         @wraps(func)
         def wrapper(self, new_pass):
             try:
                 func(self, new_pass)
             except:
-                raise Exception("No password to change!")
+                raise Exception(exc)
         return wrapper
 
     @balance_exc_decorator
     def get_account_balance(self):
+        global exc
+        exc = 'No username defined!'
+        
         return self._balance
 
     @password_exc_decorator
     def change_password(self, new_pass):
+        global exc
+        exc = 'No password to change!'
         print(f'Ваш старый пароль "{self._password}" был заменен!')
         self._password = new_pass
 
